61de3af2d7a8c7fb32810649ef7d264c56ea21fd
Owner <test>
2022-10-26 19:54:15 +0300

    refactor: delete tag selectors and add more classes  according to rule 3.4 in html-and-css.md

--- app.js
+++ app.js
@@ -33,19 +33,23 @@ var createNewTaskElement=function(taskString){
     var deleteButtonImg=document.createElement("img");//delete button image
 
     label.innerText=taskString;
-    label.className='task';
+    label.className='task label';
 
     //Each elements, needs appending
     checkBox.type="checkbox";
     editInput.type="text";
-    editInput.className="task";
+    editInput.className="task task-input";
 
     editButton.innerText="Edit"; //innerText encodes special characters, HTML does not.
-    editButton.className="edit";
+    editButton.className="edit btn";
 
-    deleteButton.className="delete";
+    deleteButton.className="delete btn";
     deleteButtonImg.src='./remove.svg';
+    deleteButtonImg.classList.add("cross");
     deleteButton.appendChild(deleteButtonImg);
+
+    listItem.classList.add("list-item")
+    checkBox.classList.add("check")
 
 
     //and appending.
